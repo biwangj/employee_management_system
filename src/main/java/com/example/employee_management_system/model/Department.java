b948640963b896ea86fc43aea4c80f495d17f3a8
JB <biwangj2525@gmail.com>
2025-03-07 15:34:26 +0800

    updated

---
+++ src/main/java/com/example/employee_management_system/model/Department.java
@@ -0,0 +1,17 @@
+package com.example.employee_management_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
